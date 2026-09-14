ae18b9a4078312d48b37e6b1add941e1baa2a487
zxcvsbnm1 <bagus4de@gmail.com>
2026-09-15 01:30:22 +0700

    Refactor bot update handling and response

--- api/index.js
+++ api/index.js
@@ -8,10 +8,9 @@ bot.on('text', (ctx) => ctx.reply('Siap Bosku! Pesan Bosku: ' + ctx.message.text
 module.exports = async (req, res) => {
   try {
     if (req.method === 'POST') {
-      await bot.handleUpdate(req.body, res);
-    } else {
-      res.status(200).send('Bot Bagus4de Ready Bosku!');
+      await bot.handleUpdate(req.body);
     }
+    res.status(200).send('OK Bosku');
   } catch (e) {
     console.error(e);
     res.status(200).send('OK Bosku');
