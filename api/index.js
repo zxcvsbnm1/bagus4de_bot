9e8d309267a585075622fa3362319e349e4b6123
zxcvsbnm1 <bagus4de@gmail.com>
2026-09-15 19:20:39 +0700

    Fix syntax error in handler function export

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-export default async function handler(req,res){
+
+export default async function handler(req, res){
   const supa = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
   const p = (req.query.pasaran || 'hk').toLowerCase()
   const { data } = await supa.from('prediksi').select('*').eq('pasaran', p).single()
